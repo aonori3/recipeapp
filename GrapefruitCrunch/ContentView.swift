00f1fbfb493db47dddda419c7e39c9170917e3b9
aoiotani <aoiohtani@gmail.com>
2024-09-30 15:20:30 -0400

    rem homeview

--- GrapefruitCrunch/ContentView.swift
+++ GrapefruitCrunch/ContentView.swift
@@ -2,27 +2,11 @@ import SwiftUI
 
 struct ContentView: View {
     @StateObject var pastRecipesManager = PastRecipesManager()
-    @State private var selectedTab = "Home"
+    @State private var selectedTab = "NewRecipe" 
     @Binding var userIsLoggedIn: Bool
 
     var body: some View {
         TabView(selection: $selectedTab) {
-            NavigationView {
-                HomeView(selectedTab: $selectedTab)
-                    .navigationBarTitle("", displayMode: .inline)
-                    .navigationBarItems(leading:
-                        Button(action: {
-                            // Add menu action here
-                        }) {
-                            Image(systemName: "line.horizontal.3")
-                        }
-                    )
-            }
-            .tabItem {
-                Image(systemName: "house.fill")
-            }
-            .tag("Home")
-
             NavigationView {
                 NewRecipeView()
             }
--- GrapefruitCrunch/NavigationBarView.swift
+++ GrapefruitCrunch/NavigationBarView.swift
@@ -1,26 +1,24 @@
 import SwiftUI
 
-
 struct NavigationBarView: View {
     @Binding var currentView: String
-    
+
     var body: some View {
         HStack {
-            Button(action: { currentView = "Home" }) {
-                Image(systemName: "house.fill")
-                    .foregroundColor(currentView == "Home" ? .primaryColor : .gray)
-            }
             Spacer()
+
             Button(action: { currentView = "NewRecipe" }) {
                 Image(systemName: "magnifyingglass")
                     .foregroundColor(currentView == "NewRecipe" ? .primaryColor : .gray)
             }
             Spacer()
+
             Button(action: { currentView = "PastRecipes" }) {
                 Image(systemName: "bookmark")
                     .foregroundColor(currentView == "PastRecipes" ? .primaryColor : .gray)
             }
             Spacer()
+
             Image(systemName: "person.circle")
                 .foregroundColor(.gray)
         }
